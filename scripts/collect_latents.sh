cd3b05b96ee6be7a9b72f32cf027288502e00dc4
Jan Schlegel <jschlegel@student.ethz.ch>
2025-06-19 23:11:36 +0200

    created separate bash scripts for latents collection

---
+++ scripts/collect_latents.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd ../mechinterp_diffusion/multistep_sae/scripts
+
+screen -S "collect_latents" bash -c "
+CUDA_VISIBLE_DEVICES=0 accelerate launch collect_latents.py \
+    --hook_names unet.down_blocks.2.attentions.0 \
+    --dataset_split train \
+    --dataset_size 50000 \
+    --batch_size 50 \
+    --num_inference_steps 25
+CUDA_VISIBLE_DEVICES=0 accelerate launch collect_latents.py \
+    --hook_names unet.down_blocks.2.attentions.0 \
+    --dataset_split train \
+    --dataset_size 10000 \
+    --batch_size 50 \
+    --num_inference_steps 25
+CUDA_VISIBLE_DEVICES=0 accelerate launch collect_latents.py \
+    --hook_names unet.up_blocks.1.attentions.1 \
+    --dataset_split train \
+    --dataset_size 50000 \
+    --batch_size 50 \
+    --num_inference_steps 25
+CUDA_VISIBLE_DEVICES=0 accelerate launch collect_latents.py \
+    --hook_names unet.up_blocks.1.attentions.1 \
+    --dataset_split test \
+    --dataset_size 10000 \
+    --batch_size 50 \
+    --num_inference_steps 25
+"
---
+++ scripts/collect_latents_for_probe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ../mechinterp_diffusion/multistep_sae/scripts
+
+screen -S "extract_probe_latents" bash -c "
+    CUDA_VISIBLE_DEVICES=0 accelerate launch collect_latents.py \
+        --hook_names unet.down_blocks.2.attentions.0 \
+        --dataset_name birds_vs_cats \
+        --dataset_split train \
+        --dataset_size 10000 \
+        --batch_size 50 \
+        --output_or_diff "output" \
+        --num_inference_steps 25 \
+        --target_timesteps_idx 4
+"
