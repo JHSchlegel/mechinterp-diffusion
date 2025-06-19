d6e13e92adf630ece8b94aaedb0d81fd600db900
Jan Schlegel <jschlegel@student.ethz.ch>
2025-06-19 23:12:04 +0200

    separate bash script for top activating examples

---
+++ scripts/collect_top_examples.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ../mechinterp_diffusion/multistep_sae/scripts
+
+screen -S "collect_top_activating_examples" bash -c "
+    python collect_top_dataset_examples.py \
+        --target_module unet.down_blocks.2.attentions.0 \
+        --num_prompts 5000 \
+        --num_examples_per_feature 9
+"
