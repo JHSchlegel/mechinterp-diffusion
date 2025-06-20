4cecc82de930799e2dcda2d4b7cc5c067b6aabe5
Jan Schlegel <jschlegel@student.ethz.ch>
2025-06-20 16:59:27 +0200

    extracted test latents

--- scripts/collect_latents_for_probe.sh
+++ scripts/collect_latents_for_probe.sh
@@ -11,4 +11,14 @@ screen -S "extract_probe_latents" bash -c "
         --output_or_diff "output" \
         --num_inference_steps 25 \
         --target_timesteps_idx 4
+
+    CUDA_VISIBLE_DEVICES=0 accelerate launch collect_latents.py \
+        --hook_names unet.down_blocks.2.attentions.0 \
+        --dataset_name birds_vs_cats \
+        --dataset_split test \
+        --dataset_size 2000 \
+        --batch_size 50 \
+        --output_or_diff "output" \
+        --num_inference_steps 25 \
+        --target_timesteps_idx 4
 "
