b5f086d4b32192158abdbbb9da30e37936f72ce0
Jan Schlegel <jschlegel@student.ethz.ch>
2025-06-22 01:03:51 +0200

    visualize ablations directly

--- scripts/run_ablations.sh
+++ scripts/run_ablations.sh
@@ -4,10 +4,25 @@ cd ../mechinterp_diffusion/experiments
 
 echo "Starting ablations..."
 
-python ablate_sae.py --config-name sweep_k --multirun
-python ablate_sae.py --config-name sweep_batch_size --multirun
-python ablate_sae.py --config-name sweep_learning_rate --multirun
-python ablate_sae.py --config-name sweep_d_sae --multirun
-python ablate_sae.py --config-name sweep_batch_topk --multirun
+screen -S "run_ablations" bash -c "
+    python ablate_sae.py --config-name sweep_k --multirun
+    python visualize_ablation_results.py --color aaas --use_std
+
+
+    python ablate_sae.py --config-name sweep_batch_size --multirun
+    python visualize_ablation_results.py --color aaas --use_std
+
+
+    python ablate_sae.py --config-name sweep_learning_rate --multirun
+    python visualize_ablation_results.py --color aaas --use_std
+
+
+    python ablate_sae.py --config-name sweep_d_sae --multirun
+    python visualize_ablation_results.py --color aaas --use_std
+
+
+    python ablate_sae.py --config-name sweep_batch_topk --multirun
+    python visualize_ablation_results.py --color aaas --use_std
+"
 
 echo "Done."
