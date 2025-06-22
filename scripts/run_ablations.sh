8bd05e322c8083d68eac4e19d7a65c538c9e257e
Jan Schlegel <jschlegel@student.ethz.ch>
2025-06-22 02:10:05 +0200

    settled on using 3 seeds

--- scripts/run_ablations.sh
+++ scripts/run_ablations.sh
@@ -5,24 +5,24 @@ cd ../mechinterp_diffusion/experiments
 echo "Starting ablations..."
 
 screen -S "run_ablations" bash -c "
+    python ablate_sae.py --config-name sweep_batch_topk --multirun
+    python visualize_sae_ablation_results.py --color aaas --use_std
+
     python ablate_sae.py --config-name sweep_k --multirun
-    python visualize_ablation_results.py --color aaas --use_std
+    python visualize_sae_ablation_results.py --color aaas --use_std
 
 
     python ablate_sae.py --config-name sweep_batch_size --multirun
-    python visualize_ablation_results.py --color aaas --use_std
+    python visualize_sae_ablation_results.py --color aaas --use_std
 
 
     python ablate_sae.py --config-name sweep_learning_rate --multirun
-    python visualize_ablation_results.py --color aaas --use_std
+    python visualize_sae_ablation_results.py --color aaas --use_std
+
 
 
     python ablate_sae.py --config-name sweep_d_sae --multirun
-    python visualize_ablation_results.py --color aaas --use_std
-
-
-    python ablate_sae.py --config-name sweep_batch_topk --multirun
-    python visualize_ablation_results.py --color aaas --use_std
+    python visualize_sae_ablation_results.py --color aaas --use_std
 "
 
 echo "Done."
