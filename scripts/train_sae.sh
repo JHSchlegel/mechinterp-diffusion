4bbcb75ee5784f64c8cf41ea2a9a514ef3033e53
Jan Schlegel <jschlegel@student.ethz.ch>
2025-08-21 19:24:22 +0200

    updated train script

--- scripts/train_sae.sh
+++ scripts/train_sae.sh
@@ -2,9 +2,9 @@
 cd ../mechinterp_diffusion/scripts
 
 screen -S "train_sae" bash -c "
+    python train_sae.py TopK --dataset_path ../../data/activations/stable-diffusion-2-1/laion/train/subset_size-50000/25-inference-steps/every-1-steps/unet.up_blocks.1.attentions.1 \
+        --checkpoint_path ../../checkpoints/sae/up_blocks.1.attentions.1/
     python train_sae.py TopK \
         --dataset_path ../../data/activations/stable-diffusion-2-1/laion/train/subset_size-50000/25-inference-steps/every-1-steps/unet.down_blocks.2.attentions.0 \
         --checkpoint_path ../../checkpoints/sae/down_blocks.2.attentions.0/
-    python train_sae.py TopK --dataset_path ../../data/activations/stable-diffusion-2-1/laion/train/subset_size-50000/25-inference-steps/every-1-steps/unet.up_blocks.1.attentions.1 \
-        --checkpoint_path ../../checkpoints/sae/up_blocks.1.attentions.1/
 "
