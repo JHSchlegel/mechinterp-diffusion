0d8e5a31108b8ec7080627223951df13be0bb49a
Jan Schlegel <jschlegel@student.ethz.ch>
2025-08-22 21:46:51 +0200

    simplified bash script

--- scripts/activation_patching.sh
+++ scripts/activation_patching.sh
@@ -2,16 +2,12 @@
 cd /media/Thesis/mechinterp-diffusion/mechinterp_diffusion/experiments/
 
 screen -S "activation_patching" bash -c "
-    python activation_patching.py
-    python activation_patching.py --patching_timestep_indices 0 1 2 3 4 5 6 7 8 9
-    python activation_patching.py --patching_timestep_indices 0 1 2 3 4
-    python activation_patching.py --patching_timestep_indices 5 6 7 8 9
-    python activation_patching.py --patching_timestep_indices 10 11 12 13 14
-    python activation_patching.py --patching_timestep_indices 15 16 17 18 19 20 21 22 23 24
-    python activation_patching.py --height 512 --width 512
-    python activation_patching.py --patching_timestep_indices 0 1 2 3 4 5 6 7 8 9 --height 512 --width 512
-    python activation_patching.py --patching_timestep_indices 0 1 2 3 4 --height 512 --width 512
-    python activation_patching.py --patching_timestep_indices 5 6 7 8 9 --height 512 --width 512
-    python activation_patching.py --patching_timestep_indices 10 11 12 13 14 --height 512 --width 512
-    python activation_patching.py --patching_timestep_indices 15 16 17 18 19 20 21 22 23 24 --height 512 --width 512
+    for res in '--height 768 --width 768' '--height 512 --width 512'; do
+        python activation_patching.py \$res
+        python activation_patching.py --patching_timestep_indices 0 1 2 3 4 5 6 7 8 9 \$res
+        python activation_patching.py --patching_timestep_indices 0 1 2 3 4 \$res
+        python activation_patching.py --patching_timestep_indices 5 6 7 8 9 \$res
+        python activation_patching.py --patching_timestep_indices 10 11 12 13 14 \$res
+        python activation_patching.py --patching_timestep_indices 15 16 17 18 19 20 21 22 23 24 \$res
+    done
 "
