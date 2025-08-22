00471a236caa2ad45901d5744be007ef5ea9af7e
Jan Schlegel <jschlegel@student.ethz.ch>
2025-08-22 20:57:43 +0200

    updated eval bash script

--- scripts/evaluate_sae.sh
+++ scripts/evaluate_sae.sh
@@ -2,12 +2,26 @@
 cd ../mechinterp_diffusion/experiments
 
 screen -S "evaluate_sae" bash -c "
-    python evaluate_sae.py \
-        --model_path ../../checkpoints/sae/TopKSAE_dsae-5120_timesteps-all_20250523_212803/step_488282 \
-        --dataset_path ../../data/activations/stable-diffusion-2-1/laion/test/subset_size-50000/25-inference-steps/every-1-steps/unet.down_blocks.2.attentions.0 \
-        --output_dir ../../results/sae_evaluation/down_blocks.2.attentions.0/ \
-        --num_samples 10000 \
-        --colors aaas \
-        --batch_size 100 \
-        --spatial_resolution 16 16
+        python evaluate_sae.py \
+                --mode reconstruction \
+                --num_prompts 1000 \
+                --seeds 42 43 44 45 46 \
+                --device cuda \
+                --torch_dtype float16 \
+                --num_inference_steps 25 \
+                --guidance_scale 9.0 \
+                --height 512 \
+                --width 512
+
+        python evaluate_sae.py \
+                --mode feature_removal \
+                --num_prompts 1000 \
+                --seeds 42 43 44 45 46 \
+                --device cuda \
+                --torch_dtype float16 \
+                --num_inference_steps 25 \
+                --guidance_scale 9.0 \
+                --height 512 \
+                --width 512
+
 "
