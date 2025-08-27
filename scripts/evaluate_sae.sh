1705031a1c7f3c1fe852702671f84f874ad3aaec
Jan Schlegel <jschlegel@student.ethz.ch>
2025-08-27 08:57:00 +0200

    updated eval script to final version

--- scripts/evaluate_sae.sh
+++ scripts/evaluate_sae.sh
@@ -15,7 +15,7 @@ screen -S "evaluate_sae" bash -c "
                 --width 512
 
 
-    python evaluate_sae.py \
+        python evaluate_sae.py \
                 --mode feature_removal \
                 --num_prompts 1000 \
                 --seeds 42 43 44 45 46 \
@@ -28,14 +28,14 @@ screen -S "evaluate_sae" bash -c "
 
 
 
-    python evaluate_sae.py \
-            --mode reconstruction \
-            --num_prompts 1000 \
-            --seeds 42 43 44 45 46 \
-            --device cuda \
-            --torch_dtype float16 \
-            --num_inference_steps 25 \
-            --guidance_scale 9.0 \
-            --height 768 \
-            --width 768
+        python evaluate_sae.py \
+                --mode reconstruction \
+                --num_prompts 1000 \
+                --seeds 42 43 44 45 46 \
+                --device cuda \
+                --torch_dtype float16 \
+                --num_inference_steps 25 \
+                --guidance_scale 9.0 \
+                --height 768 \
+                --width 768
 "
